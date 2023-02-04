2bb42296f190a2c6c50d1160d093452234c03e94
Chadie Gil <77107717+chadiegil@users.noreply.github.com>
2023-02-04 10:06:10 +0800

    plus-one

---
+++ plus-one.js
@@ -0,0 +1,16 @@
+// const digits = [1, 2, 3];
+const digits = [9];
+
+function plusOne(digits) {
+  for (let i = digits.length - 1; i >= 0; i--) {
+    digits[i]++;
+    if (digits[i] > 9) {
+      digits[i] = 0;
+    } else {
+      return digits;
+    }
+  }
+  digits.unshift(1);
+  return digits;
+}
+console.log(plusOne(digits));
